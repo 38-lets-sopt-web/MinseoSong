eb269067d983c0a071a20cf4d03aac87ca855de7
MinseoSONG <mins041600@naver.com>
2026-04-21 01:27:48 +0900

    mod; 데이터 수정

--- Week2/Homework/account-book/js/data.js
+++ Week2/Homework/account-book/js/data.js
@@ -96,7 +96,3 @@ export const initialExpenses = [
     amount: -5000,
   },
 ];
-
-if (!localStorage.getItem("expenseData")) {
-  localStorage.setItem("expenseData", JSON.stringify(initialExpenses));
-}
