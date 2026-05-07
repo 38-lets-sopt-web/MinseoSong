310ce14354bad9d9ea5fc7360f19d6f0d0e9135c
MinseoSONG <mins041600@naver.com>
2026-05-07 23:46:18 +0900

    style: 스타일 시스템 / 전역 스타일

---
+++ Week4/Homework/admin-page/src/styles/styled.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components';
+import { theme } from './theme';
+
+type AppTheme = typeof theme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends AppTheme {
+    readonly __themeBrand?: never;
+  }
+}
